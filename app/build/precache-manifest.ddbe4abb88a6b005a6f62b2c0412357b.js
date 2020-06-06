cc542e930a131c2dfe425a56603579e57bd42a91
Sveta Mituykhina <s.tu.tu.908@gmail.com>
2020-06-06 17:16:36 +0300

    add build

---
+++ app/build/precache-manifest.ddbe4abb88a6b005a6f62b2c0412357b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4206ff67b807d4d99e1a7f0dbd5d8d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca7dd2068d5b688e8466",
+    "url": "/static/css/main.b3cbd57b.chunk.css"
+  },
+  {
+    "revision": "de685b9f951818d3c47f",
+    "url": "/static/js/2.694cbfed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.694cbfed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca7dd2068d5b688e8466",
+    "url": "/static/js/main.0778c70d.chunk.js"
+  },
+  {
+    "revision": "4305ad129dcf89844c80",
+    "url": "/static/js/runtime-main.3ea18b6e.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "06f3815259207cad99fbe8d073520a4f",
+    "url": "/static/media/arrow.06f38152.svg"
+  },
+  {
+    "revision": "fcf78735f5a84cb568104b95ecffb25d",
+    "url": "/static/media/back.fcf78735.png"
+  },
+  {
+    "revision": "1fd0379c90cb200b4725b3fceec32a6b",
+    "url": "/static/media/city.1fd0379c.svg"
+  },
+  {
+    "revision": "f0a1bfbf7b7281fce6d88f7a10e55484",
+    "url": "/static/media/inic.f0a1bfbf.svg"
+  },
+  {
+    "revision": "e6bf32ed00b46056857f184f8c6854da",
+    "url": "/static/media/message.e6bf32ed.svg"
+  },
+  {
+    "revision": "78231b7eaa0e88b9a0eedb6b0d7007fd",
+    "url": "/static/media/mute.78231b7e.svg"
+  },
+  {
+    "revision": "5e97c7b35e2cbfd224eff0a426253c48",
+    "url": "/static/media/mypage.5e97c7b3.svg"
+  },
+  {
+    "revision": "7e5b72501834a3113e9d49d844cecce3",
+    "url": "/static/media/recommend.7e5b7250.svg"
+  },
+  {
+    "revision": "755d16f9601e36c2ac26ac3d34fe1a20",
+    "url": "/static/media/search.755d16f9.svg"
+  },
+  {
+    "revision": "047f27444c539ae9a06353e080f4a5b2",
+    "url": "/static/media/setting.047f2744.svg"
+  },
+  {
+    "revision": "a34bd925e78a0d237ed819eaaf9cdd6c",
+    "url": "/static/media/voices.a34bd925.svg"
+  }
+]);
